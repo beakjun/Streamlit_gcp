a670c60c4d551b36b0732d1d3f9f796eb1723e45
beakjun <wjsqorwns93@naver.com>
2023-10-20 12:10:29 +0900

    code added

--- pages/KOSPI.py
+++ pages/KOSPI.py
@@ -46,3 +46,27 @@ def run_query(query):
     return df
 query2=f"SELECT * from airflow.stock_market_tbl where \"itmsNm\" = '{option[:-8]}' order by \"basDt\" "
 df = run_query(query2)
+
+### 검색 결과 데이터 프레임 생성 
+@st.cache_data(ttl=600)
+def run_query(query):
+    df = pd.read_sql_query(query,con=conn)
+    return df
+query2=f"SELECT * from airflow.stock_market_tbl where \"itmsNm\" = '{option[:-8]}' order by \"basDt\" "
+df = run_query(query2)
+
+### class 호출
+indicators = TechnicalIndicators(df)
+indicators.preprocess_data()
+main_df=indicators.df.copy()
+
+indicators.compute_macd()
+
+indicators.compute_rsi()
+rsi_df=indicators.df
+
+placeholder1 = st.empty()
+
+placeholder1.plotly_chart(candlechart.plot_candlestick(rsi_df, '주식 캔들 차트',macd,rsi))
+
+placeholder.title(f'{option.split()[0]} 일별 주가') # 종목이름으로 타이틀
