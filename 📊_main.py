794741cac3aa5e694304db0c21cb57adf02a4227
beakjun <wjsqorwns93@naver.com>
2023-10-27 17:47:47 +0900

    metrics modified commit

--- 📊_main.py
+++ 📊_main.py
@@ -19,8 +19,24 @@ def init_connection():
 
 conn = init_connection()
 
-col1, col2, col3 =st.columns(3)
-col1.metric("KOSPI",200)
-col2.metric("KOSDAQ",100)
-col3.metric("KOSPI200",10)
+def run_query(query):
+    df = pd.read_sql_query(query,con=conn)
+    return df
 
+
+tab1, tab2, tab3, tab4 = st.tabs(["📈 상승","📉 하락" , "💸 거래상위", "💰 시가총액 상위"])
+
+with tab1:
+    df = run_query('select "itmsNm","mrktCtg", "clpr", "vs", "fltRt" from stockprice_info.stockprice_stat order by "fltRt" desc limit 10')
+    st.dataframe(df)
+    tab1.metric(f"{df['mrktCtg'].iloc[0]} $\\newline $ {df['itmsNm'].iloc[0]}",f"{df['clpr'].iloc[0]}",f"{df['fltRt'].iloc[0]}%({df['vs'].iloc[0]})",delta_color="inverse")
+with tab2:
+    df = run_query('select "itmsNm","mrktCtg", "clpr", "vs", "fltRt" from stockprice_info.stockprice_stat order by "fltRt" asc limit 10')
+    st.dataframe(df)
+    tab2.metric(f"{df['mrktCtg'].iloc[0]} $\\newline $ {df['itmsNm'].iloc[0]}",f"{df['clpr'].iloc[0]}",f"{df['fltRt'].iloc[0]}%({df['vs'].iloc[0]})",delta_color="inverse")
+with tab3:
+    df = run_query('select "itmsNm","mrktCtg", "clpr", "vs", "fltRt" from stockprice_info.stockprice_stat order by "trqu" desc limit 10')
+    st.dataframe(df)
+with tab4:
+    df = run_query('select "itmsNm","mrktCtg", "clpr", "vs", "fltRt" from stockprice_info.stockprice_stat order by "mrktTotAmt" desc limit 10')
+    st.dataframe(df)
