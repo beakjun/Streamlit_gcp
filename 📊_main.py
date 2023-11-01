078f35e9e0ab28aa559f3f54486aec38f367ce59
beakjun <wjsqorwns93@naver.com>
2023-11-02 02:50:01 +0900

    index graph complete

--- 📊_main.py
+++ 📊_main.py
@@ -4,7 +4,7 @@ import numpy as np
 import psycopg2
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-
+import datetime
 
 # 페이지 설정
 st.set_page_config(page_title="📊주가 현황", layout="wide")
@@ -12,72 +12,118 @@ st.set_page_config(page_title="📊주가 현황", layout="wide")
 # 메인 페이지 타이틀
 st.title("📊주가 현황")
 
+# 공백 생성
+st.write("")
 
+# DB 연결 함수
 @st.cache_resource
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
+# 쿼리 생성 함수
 @st.cache_data
 def run_query(query):
     df = pd.read_sql_query(query,con=conn)
     return df
+
+# 데이터 날짜 추출
 with init_connection() as conn:
     dt = run_query('select "basDt" from stockprice_info.stockprice_stat limit 1').iat[0,0]
-
 md=dt.strftime("%m.%d")
 ymd=dt.strftime("%Y.%m.%d")
 
+# 하위 제목
+st.subheader(f"주요 증시 현황 ({md})",divider='grey')
 
-st.subheader(f"주요 증시 현황 ({md})")
+# 크롤링 URL 정의
+kospi_url = 'https://finance.naver.com/sise/sise_index_day.naver?code=KOSPI&page='
+kospi200_url = 'https://finance.naver.com/sise/sise_index_day.naver?code=KPI200&page='
+kosdaq_url ='https://finance.naver.com/sise/sise_index_day.naver?code=KOSDAQ&page='
 
-kospi_url = 'https://finance.naver.com/sise/sise_index_day.naver?code=KOSPI&page=1'
-kospi200_url = 'https://finance.naver.com/sise/sise_index_day.naver?code=KPI200&page=1'
-kosdaq_url ='https://finance.naver.com/sise/sise_index_day.naver?code=KOSDAQ&page=1'
 
 def display_metric2(url,ymd,col,w):
+    url = url+'1'
     df = pd.read_html(url,encoding='cp949')[0]
     df = df[df['날짜']==ymd]
     title = w
-    value = df['체결가']
+    value = f"{df['체결가'].iat[0]:,}"
     delta = f"{df['등락률'].iat[0]}({df['전일비'].iat[0]})"
     col.metric(title, value, delta, delta_color="inverse")
+    return delta
+
+
+### 그래프 크게 보기 표시 제거
+hide_img_fs = '''
+<style>
+button[title="View fullscreen"]{
+    visibility: hidden;}
+</style>
+'''
+
+st.markdown(hide_img_fs, unsafe_allow_html=True)
+
+
+def display_chart(url,col,days=90):
+    ## 날짜 빼는 부분 약 3달
+    odt = dt- datetime.timedelta(days=days) 
+    oymd = odt.strftime("%Y.%m.%d")
+
+    ## 20페이지 정도 불러와서 df 에 저장하고
+    new_df = pd.DataFrame()
+    for i in range(1,int(days/3)):
+        new_url = url+str(i)
+        df = pd.read_html(new_url,encoding='cp949')[0]
+        new_df = pd.concat([new_df,df],ignore_index=True)
+    ## NaN 제거 및 불러온 날짜까지만
+    new_df=new_df.dropna()
+
+    graph_df = new_df[(new_df['날짜']>=oymd)&(new_df['날짜']<=ymd)].sort_values(by='날짜',ascending=True)
+    ## 데이터 준비 끝
+
+    #if graph_df[graph_df['날짜']==ymd]['등락률'][0]=='-':
+    ## 파랑불일지 빨간 불일지 판단
+    #pass
+    if graph_df[graph_df['날짜']==ymd]['등락률'].iloc[0][0]=='-':
+        color = '#007aff'
+    else : 
+        color = '#e52300'
     
+    # 그래프 
+    fig, ax = plt.subplots(figsize=(12, 5))
+    ax.plot(graph_df['날짜'], graph_df['체결가'], color = color ,linewidth=3.5, alpha=0.7)
+    poly= ax.fill_between(graph_df['날짜'], graph_df['체결가'], graph_df['체결가'].min(), alpha=0.05, color =color )
+    ylim = ax.get_ylim()
+    ax.spines['top'].set_alpha(0)
+    ax.spines['right'].set_alpha(0)
+    ax.spines['bottom'].set_alpha(0.1)
+    ax.spines['left'].set_alpha(0.1)
 
-col1,col2,col3,col4,col5,col6,col7,col8=st.columns(8)
-display_metric2(kospi_url,ymd,col2,"코스피")
-display_metric2(kosdaq_url,ymd,col5,"코스닥")
+    plt.xticks([])
+    plt.yticks([])
+    
+    col.pyplot(fig)
 
 
-cols1,cols2,gap1,cols3,cols4=st.columns([0.1,1,0.2,1,1])
-np.random.seed(123)
-x = np.linspace(0, 10, 200)
-y = np.random.normal(0.01, 1, 200).cumsum()
-fig, ax = plt.subplots(figsize=(12, 5))
-ax.plot(x, y,color='white')
 
-ylim = ax.get_ylim()
+### 지스 메트릭 
+col1,colgap1,col2,colgap2,col3=st.columns([1,0.2,1,0.2,1])
+display_metric2(kospi_url,ymd,col1,"코스피")
+display_metric2(kosdaq_url,ymd,col2,"코스닥")
+display_metric2(kospi200_url,ymd,col3,"코스피200")
 
-grad1 = ax.imshow(np.linspace(0, 1, 256).reshape(-1, 1), cmap='Reds', vmin=-0.05, aspect='auto',
-                  extent=[x.min(), x.max(), 0, y.max()], origin='lower')
-poly_pos = ax.fill_between(x, y.min(), y, alpha=0.05)
-grad1.set_clip_path(poly_pos.get_paths()[0], transform=ax.transData)
-poly_pos.remove()
-plt.axis('off')
-grad2 = ax.imshow(np.linspace(0, 1, 256).reshape(-1, 1), cmap='Blues', vmin=-0.5, aspect='auto',
-                  extent=[x.min(), x.max(), y.min(), 0], origin='upper')
-poly_neg = ax.fill_between(x, y, y.max(), alpha=0.1)
-grad2.set_clip_path(poly_neg.get_paths()[0], transform=ax.transData)
-poly_neg.remove()
 
-cols2.pyplot(fig)
-cols3.pyplot(fig)
+
+### 그래프
+display_chart(kospi_url,col1)
+display_chart(kosdaq_url,col2)
+display_chart(kospi200_url,col3)
 
 st.write("")
 st.write("")
 
 def display_metrics(column, df,idx):
     title = f"{df['mrktCtg'].iloc[idx]} $\\newline$ {df['itmsNm'].iloc[idx]}"
-    value = f"{df['clpr'].iloc[idx]}"
+    value = f"{df['clpr'].iloc[idx]:,}"
     delta = f"{df['fltRt'].iloc[idx]}%({df['vs'].iloc[idx]})"
     column.metric(title, value, delta, delta_color="inverse")
 
